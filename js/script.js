691941f42c923359d7b15196cca237bcb6a1e609
Julyanderson Alves <ansderson84001@gmail.com>
2021-12-23 14:09:11 -0300

    animacoes

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 
 function start(){
     $("#inicio").hide();
-    $("#fundoGame").append("<div id='jogador'></div>")
-    $("#fundoGame").append("<div id='inimigo1'></div>")
+    $("#fundoGame").append("<div id='jogador' class='animal'></div>")
+    $("#fundoGame").append("<div id='inimigo1' class='animal2'></div>")
     $("#fundoGame").append("<div id='inimigo2'></div>")
-    $("#fundoGame").append("<div id='amigo'></div>")
+    $("#fundoGame").append("<div id='amigo' class='animal3'></div>")
     
 }
