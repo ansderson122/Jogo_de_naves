4453d5c7e8de3fdef457a6d7b6dbfad14a3a83c7
Julyanderson Alves <ansderson84001@gmail.com>
2021-12-24 17:19:17 -0300

    dispora

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function start(){
     var jogo = {}
     var velocidade = 5 
     var possicaoY = parseInt(Math.random() * 334)
+    var podeAtirar = true
 
     var tecla = {
         w:87,
@@ -58,6 +59,7 @@ function start(){
             
         }
         if(jogo.pressionou[tecla.d]){
+            disparo()
             
         }
     }
@@ -76,13 +78,46 @@ function start(){
 
     function moveinimigo2(){
         possicaoX = parseInt($("#inimigo2").css("left"))
-        $("#inimigo2").css("left",possicaoX-3)
+        let velocidadeInimigo2 = 3
+        $("#inimigo2").css("left",possicaoX-velocidadeInimigo2)
 
         if(possicaoX<=0){
             $("#inimigo2").css("left",775)
         }
+    }
+
+    function disparo(){
+        if (podeAtirar === true){
+            podeAtirar = false
+
+            let topo = parseInt($("#jogador").css("top"))
+            let possicaoX = parseInt($("#jogador").css("left"))
+            let tiroX = possicaoX + 190
+            let topoTiro = topo + 37
+
+            $("#fundoGame").append("<div id ='disparo'></div>")
+            $("#disparo").css("top", topoTiro)
+            $("#disparo").css("left",tiroX)
+
+            var tempoDisparo = setInterval(function(){
+                let possicaoX = parseInt($("#disparo").css("left"))
+                let velocidadeDisparo = 45
+                $("#disparo").css("left",possicaoX + velocidadeDisparo)
+
+                if (possicaoX > 900){
+                    window.clearInterval(tempoDisparo)
+                    tempoDisparo = null
+                    $("#disparo").remove()
+                    podeAtirar = true
+                }
+            },30) 
+        }
+
+        
 
     }
+
+
 
 }
 
