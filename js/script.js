cd253938d6143a683c3eda1401431fb8b8af475f
Julyanderson Alves <ansderson84001@gmail.com>
2021-12-24 10:17:03 -0300

    movimento

--- js/script.js
+++ js/script.js
@@ -8,15 +8,54 @@ function start(){
     $("#fundoGame").append("<div id='amigo' class='animal3'></div>")
 
     var jogo = {}
+    var tecla = {
+        w:87,
+        s:83,
+        d:68
+    }
+    jogo.pressionou = []
+
+    $(document).keydown(function(e){
+        jogo.pressionou[e.which] = true
+    })
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false
+    })
+
+
+
     jogo.timer = setInterval(loop,30)
 
     function loop(){
         movefundo()
+        movejogador()
     }
 
     function movefundo(){
         esquerda = parseInt($("#fundoGame").css("background-position"))
         $("#fundoGame").css("background-position",esquerda-1)
+    }
+
+    function movejogador(){
+        if(jogo.pressionou[tecla.w]){
+            let topo = parseInt($("#jogador").css("top"))
+            if (topo>10){
+                $("#jogador").css("top",topo-10)
+            }
+            
+
+        }
+        if(jogo.pressionou[tecla.s]){
+            let topo = parseInt($("#jogador").css("top"))
+            if (topo<434){
+                $("#jogador").css("top",topo+10)
+            }
+            
+        }
+        if(jogo.pressionou[tecla.d]){
+            
+        }
+
     }
 
 }
